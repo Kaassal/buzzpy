69cc73e5cf4acb5a02007126643b1c4c41385587
Diego Casal Carballal <diegocasal2003@gmail.com>
2025-04-11 21:07:52 +0200

    FEAT:Code quality improvents

--- ssh_honeypot.py
+++ ssh_honeypot.py
@@ -1,34 +1,36 @@
-#Everlyline with this comment "Change this" can and should be changed
-#if you want people to interact with the honeypot
-
 # Import libraries
 import logging
 from logging.handlers import RotatingFileHandler
 import socket
-import paramiko
 import threading
+import paramiko
 
-# Constant vars
-logging_format = logging.Formatter('%(message)s')
-# The banner can be changed, it is just a string, a vulnerable version has been selected to lure auditors and/or attackers
-SSH_BANNER = "SSH-2.0-OpenSSH_6.6.1p1 Ubuntu-2ubuntu2 honeypot"  # Change this (delete honeypot)
-host_key = paramiko.RSAKey(filename='server.key')
+# Constant variables
+LOGGING_FORMAT = logging.Formatter('%(message)s')
+SSH_BANNER = "SSH-2.0-OpenSSH_6.6.1p1 Ubuntu-2ubuntu2"  # Updated to remove 'honeypot'
+HOST_KEY = paramiko.RSAKey(filename='server.key')
 
 # Logging setup for audit and command logs
-funnel_logger = logging.getLogger('FunnelLogger')
-funnel_logger.setLevel(logging.INFO)
-funnel_handler = RotatingFileHandler('audits.log', maxBytes=2000, backupCount=5)
-funnel_handler.setFormatter(logging_format)
-funnel_logger.addHandler(funnel_handler)
+FUNNEL_LOGGER = logging.getLogger('FunnelLogger')
+FUNNEL_LOGGER.setLevel(logging.INFO)
+FUNNEL_HANDLER = RotatingFileHandler('audits.log', maxBytes=2000, backupCount=5)
+FUNNEL_HANDLER.setFormatter(LOGGING_FORMAT)
+FUNNEL_LOGGER.addHandler(FUNNEL_HANDLER)
 
-creds_logger = logging.getLogger('CmdLogger')
-creds_logger.setLevel(logging.INFO)
-creds_handler = RotatingFileHandler('cmd_audits.log', maxBytes=2000, backupCount=5)
-creds_handler.setFormatter(logging_format)
-creds_logger.addHandler(creds_handler)
+CREDS_LOGGER = logging.getLogger('CmdLogger')
+CREDS_LOGGER.setLevel(logging.INFO)
+CREDS_HANDLER = RotatingFileHandler('cmd_audits.log', maxBytes=2000, backupCount=5)
+CREDS_HANDLER.setFormatter(LOGGING_FORMAT)
+CREDS_LOGGER.addHandler(CREDS_HANDLER)
 
-# Shell emulation
 def emulated_shell(channel, client_ip):
+    """
+    Emulates a shell environment for the SSH honeypot.
+
+    Args:
+        channel (paramiko.Channel): The SSH channel.
+        client_ip (str): The IP address of the client.
+    """
     channel.send(b'ssh-honeypot$ ')
     command = b""
     while True:
@@ -36,36 +38,39 @@ def emulated_shell(channel, client_ip):
         channel.send(char)
         if not char:
             channel.close()
+            break
 
         command += char
-        # TODO: Find a more elegant way of doing data collection
         if char == b'\r':
-            if command.strip() == b'exit':
-                response = b'\n Connection terminated \n'
+            command = command.strip()
+            if command == b'exit':
+                response = b'\nConnection terminated\n'
+                channel.send(response)
                 channel.close()
-            elif command.strip() == b'pwd':
-                response = b"\n" + b'\\usr\\local' + b'\r\n'
-                creds_logger.info(f'Command {command.strip()} Client: {client_ip}')
-            elif command.strip() == b'whoami':
-                response = b"\n" + b"honeypotUser1" + b"\r\n"  # Change this
-                creds_logger.info(f'Command {command.strip()} Client: {client_ip}')
-            elif command.strip() == b'ls':
-                response = b'\n' + b"sshHoneypot.conf" + b"\r\n"  # Change this
-                creds_logger.info(f'Command {command.strip()} Client: {client_ip}')
-            elif command.strip() == b'cat Honeypot.conf':  # Change this
-                response = b'\n' + b"cat placeholder" + b"\r\n"
-                creds_logger.info(f'Command {command.strip()} Client: {client_ip}')
+                break
+            elif command == b'pwd':
+                response = b"\n/usr/local\r\n"
+            elif command == b'whoami':
+                response = b"\nhoneypotUser1\r\n"
+            elif command == b'ls':
+                response = b'\nsshHoneypot.conf\r\n'
+            elif command == b'cat Honeypot.conf':
+                response = b'\ncat placeholder\r\n'
             else:
-                response = b"\n" + bytes(command.strip()) + b"\r\n"
-                creds_logger.info(f'Command {command.strip()} Client: {client_ip}')
+                response = b"\n" + command + b"\r\n"
+
+            CREDS_LOGGER.info('Command: %s Client: %s', command, client_ip)
             channel.send(response)
             channel.send(b'ssh-honeypot$ ')
             command = b""
 
-# SSH Server and Sockets
 class Server(paramiko.ServerInterface):
+    """
+    Implements the SSH server interface for the honeypot.
+    """
 
     def __init__(self, client_ip, input_username=None, input_password=None):
+        super().__init__()
         self.event = threading.Event()
         self.client_ip = client_ip
         self.input_username = input_username
@@ -74,20 +79,23 @@ class Server(paramiko.ServerInterface):
     def check_channel_request(self, kind, chanid):
         if kind == 'session':
             return paramiko.OPEN_SUCCEEDED
+        return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
 
     def get_allowed_auths(self, username):
         return "password"
 
     def check_auth_password(self, username, password):
-        funnel_logger.info(f'Client {self.client_ip} connection attempt username: {username}, password: {password}')
-        creds_logger.info(f'{self.client_ip},{username},{password}')
+        FUNNEL_LOGGER.info(
+            'Client %s connection attempt username: %s, password: %s',
+            self.client_ip, username, password
+        )
+        CREDS_LOGGER.info('%s,%s,%s', self.client_ip, username, password)
+
         if self.input_username is not None and self.input_password is not None:
             if username == self.input_username and password == self.input_password:
                 return paramiko.AUTH_SUCCESSFUL
-            else:
-                return paramiko.AUTH_FAILED
-        else:
-            return paramiko.AUTH_SUCCESSFUL
+            return paramiko.AUTH_FAILED
+        return paramiko.AUTH_SUCCESSFUL
 
     def check_channel_shell_request(self, channel):
         self.event.set()
@@ -97,31 +105,41 @@ class Server(paramiko.ServerInterface):
         return True
 
     def check_channel_exec_request(self, channel, command):
-        command = str(command)
         return True
 
 def client_handle(client, addr, username, password):
+    """
+    Handles client connections to the SSH honeypot.
+
+    Args:
+        client (socket.socket): The client socket.
+        addr (tuple): The client address.
+        username (str): The username for authentication.
+        password (str): The password for authentication.
+    """
     client_ip = addr[0]
     print(f"{client_ip} connected to honeypot")
 
     try:
         transport = paramiko.Transport(client)
-        transport.local_version = SSH_BANNER  # TODO: Custom banner likely a user-enumerable vulnerable version of SSH
+        transport.local_version = SSH_BANNER
         server = Server(client_ip=client_ip, input_username=username, input_password=password)
 
-        transport.add_server_key(host_key)
+        transport.add_server_key(HOST_KEY)
         transport.start_server(server=server)
         channel = transport.accept(100)
+
         if channel is None:
             print("No channel was opened!")
+            return
 
-        standard_banner = "Connection successful! Welcome to the Honeypot!"  # Change this
-        channel.send(standard_banner)
+        welcome_message = "Connection successful! Welcome to the Honeypot!"
+        channel.send(welcome_message.encode())
         emulated_shell(channel, client_ip=client_ip)
 
     except AttributeError as error:
         print(error)
-        print("Error: Attribute error")  # TODO: better/more verbose error messages
+        print("Error: Attribute error")
     finally:
         try:
             transport.close()
@@ -130,20 +148,29 @@ def client_handle(client, addr, username, password):
             print("Error: Could not close connection!")
         client.close()
 
-# SSH-based honeypot provisions
 def honeypot(address, port, username, password):
-    sockets = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sockets.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sockets.bind((address, port))
+    """
+    Sets up the SSH honeypot server.
 
-    sockets.listen(50)  # Number of connections, anything over 100 is unstable
-    print(f"SSH server listening, port: {port}")
+    Args:
+        address (str): The IP address to bind.
+        port (int): The port to bind.
+        username (str): The username for authentication.
+        password (str): The password for authentication.
+    """
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((address, port))
+
+    server_socket.listen(50)
+    print(f"SSH server listening on port: {port}")
 
     while True:
         try:
-            client, addr = sockets.accept()
-            ssh_honeypot_thread = threading.Thread(target=client_handle, args=(client, addr, username, password))
+            client, addr = server_socket.accept()
+            ssh_honeypot_thread = threading.Thread(
+                target=client_handle, args=(client, addr, username, password)
+            )
             ssh_honeypot_thread.start()
         except Exception as error:
             print(error)
-
